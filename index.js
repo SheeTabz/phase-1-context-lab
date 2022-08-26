2bd9762e38577b7dcc7b9abb9e8c8b60f956c78d
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-26 14:10:40 +0300

    assignment

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(arr){
+  const employee ={
+   firstName : arr[0],
+   familyName : arr[1],
+   title : arr[2],
+   payPerHour : arr[3],
+   timeInEvents :[],
+   timeOutEvents : [],
+  }
+   return employee
+}
+let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
+console.log(testEmployee.firstName, testEmployee.familyName, testEmployee.title, testEmployee.payPerHour, testEmployee.firstName)
+
+
+function createEmployeeRecords(arr){
+    for (let item of arr){
+        item = createEmployeeRecord(item)
+        console.log(item)
+    }
+}
+createEmployeeRecords([])
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
